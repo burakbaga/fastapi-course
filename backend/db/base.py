3530156aa1f7157b9b20b7ef190bf0cb8d0cdb09
Burak <bagataburak@gmail.com>
2021-10-15 19:34:56 +0300

    section2code

---
+++ backend/db/base.py
@@ -0,0 +1,4 @@
+from db.base_class import Base
+from db.models.jobs import Job
+from db.models.users import User
+
--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,12 @@ from sqlalchemy.engine import create_engine
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
-from db.base_class import Base
+from db.base import Base
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
-    
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
